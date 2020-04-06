8dcb5617bca71b7299e5411cf40c09eac784326a
Jennifer <jsmit032@ucr.edu>
2020-04-05 21:07:06 -0500

    add DELETE method to delete all articles in the /articles route. Saved api docs to docs.js

--- app.js
+++ app.js
@@ -47,6 +47,16 @@ app.post('/articles', function(req, res){
   });
 });
 
+app.delete('/articles', function(req, res){
+  Article.deleteMany({}, function(err){
+    if (!err) {
+      res.redirect('/articles');
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
---
+++ docs.js
@@ -0,0 +1,28 @@
+[
+    {
+        "_id": "5e8a7a652730c1f31fad26a4",
+        "title": "REST",
+        "content": "REST is short for Representational State Transfer. It's an artitectural style for designing APIs."
+    },
+    {
+        "_id": "5e8a7c562730c1f31fad26e8",
+        "title": "Bootstrap",
+        "content": "This is a framework developed by Twitter that contains pre-made templates for web designers to build beautiful, responsive websites, quickly."
+    },
+    {
+        "_id": "5e8a7c562730c1f31fad26eb",
+        "title": "DOM",
+        "content": "The Document Object Model is like an API for interacting with HTML."
+    },
+    {
+        "_id": "5e8a7d142730c1f31fad2712",
+        "title": "API",
+        "content": "API stands for Application Programming Interface. It's a set of subroutine definitions, communication protocols, and tools for building softwear. In general terms, it is a set of clearly defined methods of communication among various components. A good API makes it easier to develop a computer program by providing all the building blocks, which are then put together by the programmer."
+    },
+    {
+        "_id": "5e8a8bae665dd5cfc62e4eef",
+        "title": "Node.js",
+        "content": "An open-source, cross-platform, JavaScript runtime environment that executes JavaScript code outside of a web browser.",
+        "__v": 0
+    }
+]
